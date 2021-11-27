87c13444553e745f94b379bd2d6ea8a754cde44b
laxman-taur9 <laxmantaur9@gmail.com>
2021-11-27 23:30:30 +0530

    Added UC9

---
+++ storeDaily.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+#Constants
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMP_RATE_PER_HOUR=150
+TOTAL_WORKING_DAYS=5
+MAX_HOURS_IN_MONTH=50
+
+
+#Variables
+totalEmpHrs=0
+totalWorkingDays=0
+totalSalary=0
+dailyWage=0
+
+
+declare -A empWageDict
+
+
+function getWorkingHours() {
+   empCheck=$((RANDOM%3))
+
+
+   case $empCheck in
+      $IS_FULL_TIME) workingHrs=8 ;;
+      $IS_PART_TIME) workingHrs=4 ;;
+      *) workingHrs=0
+   esac
+}
+
+
+while [[ $totalEmpHrs -lt $MAX_HOURS_IN_MONTH
+      && $totalWorkingDays -lt $TOTAL_WORKING_DAYS ]]
+do
+
+
+   ((totalWorkingDays++))
+   getWorkingHours
+   dailyWage=$((workingHrs*EMP_RATE_PER_HOUR))
+   empWageDict[$totalWorkingDays]=$dailyWage
+
+
+   totalEmpHrs=$((totalEmpHrs+workingHrs))
+done
+
+
+totalSalary=$((totalEmpHrs*EMP_RATE_PER_HOUR))
+
+
+empWageDict[totalSalary]=$totalSalary
+
+
+for key in ${!empWageDict[@]}
+do
+   echo $key " : " ${empWageDict[$key]}
+done
+
+
+#echo "Total Working Days: " $totalWorkingDays
+#echo "Total Working Hours: " $totalEmpHrs
+#echo "Monthly Salary is: " ${empWageDict[totalSalary]}
